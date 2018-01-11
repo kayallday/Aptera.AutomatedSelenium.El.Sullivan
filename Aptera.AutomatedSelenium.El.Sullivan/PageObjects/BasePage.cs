b138ac058d7a8e31b0bca97f6f7f5eae2978ddd8
KayBeard <kbeard@apterainc.com>
2018-01-11 14:30:51 -0500

    Add project files.

---
+++ Aptera.AutomatedSelenium.El.Sullivan/PageObjects/BasePage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aptera.AutomatedSelenium.El.Sullivan
+{
+    public class LogHelper
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
